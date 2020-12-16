90cceb8c50871a6b249e55301a8248cf30fba3a0
Diex <aboiledtiger@gmail.com>
2020-12-16 10:16:29 -0300

    makerjs added on overlay div

--- src/tools/annotation/chestWallTool/addNewMeasurement.js
+++ src/tools/annotation/chestWallTool/addNewMeasurement.js
@@ -20,6 +20,9 @@ import { moveHandle, moveNewHandle } from './../../../manipulators/index.js';
 
 import { getLogger } from '../../../util/logger.js';
 import triggerEvent from '../../../util/triggerEvent.js';
+import makerjs from 'makerjs';
+
+// import * as sbp from 'svg-blueprint';
 
 const logger = getLogger('eventDispatchers:mouseEventHandlers');
 
@@ -100,6 +103,92 @@ export default function(evt, interactionType) {
       }
     }
   );
+
+  var viewSvgContainer;
+  viewSvgContainer = document.querySelector('div.ViewportOverlay');
+
+  var processed = {
+    error: '',
+    html: '',
+    kit: null,
+    model: null,
+    measurement: null,
+    paramValues: [],
+  };
+
+  var renderOptions = {
+    origin: [512, 384],
+    annotate: true,
+    flow: { size: 8 },
+    svgAttrs: {
+      id: 'drawing',
+      style: 'margin-left:' + 0 + 'px; margin-top:' + 0 + 'px',
+      stroke: 'white',
+      fill: 'white',
+    },
+    strokeWidth: 2 + 'px',
+    fontSize: 14 + 'px',
+    scale: 100,
+    useSvgPathOnly: false,
+  };
+
+  var StraightFace = /** @class */ (function() {
+    function StraightFace() {
+      this.paths = {
+        head: new makerjs.paths.Circle([0, 0], 85),
+        eye1: new makerjs.paths.Circle([-25, 25], 10),
+        eye2: new makerjs.paths.Circle([25, 25], 10),
+        mouth: new makerjs.paths.Line([-30, -30], [30, -30]),
+      };
+    }
+    return StraightFace;
+  })();
+
+  processed.model = new StraightFace();
+
+  var maker = document.createElement('div'); // Create a <li> node
+  // maker.style.setProperty('position', 'absolute');
+  maker.className = 'top-left overlay-element';
+  maker.innerHTML = makerjs.exporter.toSVG(processed.model, renderOptions);
+
+  viewSvgContainer.appendChild(maker);
+
+  // if (overlay.visible === false) {
+  //   return;
+  // }
+
+  // const layerCanvas = document.createElement('canvas');
+
+  // layerCanvas.width = imageWidth;
+  // layerCanvas.height = imageHeight;
+
+  // const layerContext = layerCanvas.getContext('2d');
+
+  // layerContext.fillStyle = overlay.fillStyle || 'white';
+
+  // if (overlay.type === 'R') {
+  //   layerContext.fillRect(0, 0, layerCanvas.width, layerCanvas.height);
+  //   layerContext.globalCompositeOperation = 'xor';
+  // }
+
+  // let i = 0;
+
+  // for (let y = 0; y < overlay.rows; y++) {
+  //   for (let x = 0; x < overlay.columns; x++) {
+  //     if (overlay.pixelData[i++] > 0) {
+  //       layerContext.fillRect(x, y, 1, 1);
+  //     }
+  //   }
+  // }
+
+  // // Guard against non-number values
+  // const overlayX =
+  //   !isNaN(parseFloat(overlay.x)) && isFinite(overlay.x) ? overlay.x : 0;
+  // const overlayY =
+  //   !isNaN(parseFloat(overlay.y)) && isFinite(overlay.y) ? overlay.y : 0;
+  // // Draw the overlay layer onto the canvas
+
+  // canvasContext.drawImage(layerCanvas, overlayX, overlayY);
 }
 
 const checkPixelSpacing = image => {
